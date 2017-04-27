1f0a6f73c38bf90bfddca6621bc0d1b50a754aaa
Hack Reactor Student <student@hackreactor.com>
2017-04-26 19:40:44 -0700

    new

--- database/index.js
+++ database/index.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 var repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: String,
+  name: String,
+  login: String
 });
 
 var Repo = mongoose.model('Repo', repoSchema);
